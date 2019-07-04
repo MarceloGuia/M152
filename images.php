0ff9da0ddb22238a2a6765662cf8424f7eb7846e
MarceloGuia <marcelo.guia.mg@gmail.com>
2019-07-04 21:17:57 +0200

    Adapted a code I found to work with what we need, but not finished

--- images.php
+++ images.php
@@ -2,29 +2,49 @@
 
 class ImageManagement
 {
-  public function compressImages($directory, $destination)
+  public function compressImages($directory, $destination, $quality)
   {
     $list = scandir($directory);
 
-    $size = getimagesize($source);
-
-    if ($size['mime'] == 'image/jpeg')
+    for ($i = 0; $i < count($list); $i++)
     {
-      $image = imagecreatefromjpeg($source);
-    }
-    else if ($size['mime'] == 'image/gif')
-    {
-      $image = imagecreatefromgif($source);
-    }
-    else if ($size['mime'] == 'image/png')
-    {
-      $image = imagecreatefrompng($source);
+      if ($i != 0 && $i != 1)
+      {
+        $size = getimagesize($list[$i]);
+
+        if ($size['mime'] == 'image/jpeg')
+        {
+          $image = imagecreatefromjpeg($source);
+        }
+        else if ($size['mime'] == 'image/gif')
+        {
+          $image = imagecreatefromgif($source);
+        }
+        else if ($size['mime'] == 'image/png')
+        {
+          $image = imagecreatefrompng($source);
+        }
+
+        imagejpeg($image, $destination, $quality);
+      }
     }
 
-    imagejpeg($image, $destination, $quality);
-
-    return $destination;
+    return "Success!";
   }
 }
-
 ?>
+
+<!DOCTYPE html>
+<html dir="ltr">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+    <title>Notifications</title>
+    <link rel="stylesheet" href="CSS/CSS.css">
+  </head>
+  <body>
+    <?php
+    $bob = new ImageManagement();
+    echo $bob->compressImages("");
+    ?>
+  </body>
+</html>
